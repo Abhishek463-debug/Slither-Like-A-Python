9ae9aa71c6ed0a35233aac820cbf061425f0b113
Abhishek <91598690+Abhishek463-debug@users.noreply.github.com>
2022-08-06 17:09:59 +0530

    Add files via upload

---
+++ importantthingsinpython.py
@@ -0,0 +1,3 @@
+print("The most important programming concepts to learn in Python include: Data Structures and Algorithms, Data Types, Control Flow, and Concurrency.")
+
+print("The most important data libraries are numpy, pandas, matplotlib, seaborn, theano, scipy, scikit-learn, keras, tensorflow, and a few more!")
