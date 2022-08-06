141a7507d95e23785c48b4ff7ebf40c2b9d409b5
Abhishek <91598690+Abhishek463-debug@users.noreply.github.com>
2022-08-06 17:04:31 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    if 1:
+        print("I'm restarting coding! Python, Java, C++, Rust are my favorite languages!")
+    for i in range(10, 1000):
+        print_hi("Abhishek")
+
+    a_string = "Hello World"
+    print(a_string[:4])
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
